8e2a070e6663fedc9eccf3cb70e7949f3d3ec886
ekes <ekes@iskra.net>
2023-05-25 13:36:59 +0200

    Can we move the entity temporarily into another module for the period of migration.

---
+++ modules/localgov_geo_update/src/Entity/LocalgovGeo.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace Drupal\localgov_geo\Entity;
+
+use Drupal\Component\Render\PlainTextOutput;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\RevisionableContentEntityBase;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\localgov_geo\LocalgovGeoInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the geo entity class.
+ *
+ * @ContentEntityType(
+ *   id = "localgov_geo",
+ *   label = @Translation("Geo"),
+ *   label_collection = @Translation("Geos"),
+ *   bundle_label = @Translation("Geo type"),
+ *   base_table = "localgov_geo",
+ *   data_table = "localgov_geo_field_data",
+ *   revision_table = "localgov_geo_revision",
+ *   revision_data_table = "localgov_geo_field_revision",
+ *   translatable = TRUE,
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "revision" = "revision_id",
+ *     "langcode" = "langcode",
+ *     "bundle" = "bundle",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   revision_metadata_keys = {
+ *     "revision_user" = "revision_uid",
+ *     "revision_created" = "revision_timestamp",
+ *     "revision_log_message" = "revision_log"
+ *   },
+ *   bundle_entity_type = "localgov_geo_type",
+ * )
+ */
+class LocalgovGeo extends RevisionableContentEntityBase implements LocalgovGeoInterface {
+
+  use EntityChangedTrait;
+
+  /**
+   * {@inheritdoc}
+   *
+   * When a new geo entity is created, set the uid entity reference to
+   * the current user as the creator of the entity.
+   */
+  public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
+    parent::preCreate($storage_controller, $values);
+    $values += ['uid' => \Drupal::currentUser()->id()];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEnabled() {
+    return (bool) $this->get('status')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setStatus($status) {
+    $this->set('status', $status);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setCreatedTime($timestamp) {
+    $this->set('created', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('uid')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwnerId() {
+    return $this->get('uid')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwnerId($uid) {
+    $this->set('uid', $uid);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('uid', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function presave(EntityStorageInterface $storage) {
+    $token_template = $this->get('bundle')->entity->labelToken();
+    if (!empty($token_template)) {
+      $token_service = \Drupal::token();
+      $label_html = $token_service->replace($token_template, ['localgov_geo' => $this]);
+      $this->set('label', PlainTextOutput::renderFromHtml($label_html));
+    }
+    parent::preSave($storage);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['status'] = BaseFieldDefinition::create('boolean')
+      ->setRevisionable(TRUE)
+      ->setLabel(t('Status'))
+      ->setDescription(t('A boolean indicating whether the geo is enabled.'))
+      ->setDefaultValue(TRUE)
+      ->setSetting('on_label', 'Enabled')
+      ->setDisplayOptions('form', [
+        'type' => 'boolean_checkbox',
+        'settings' => [
+          'display_label' => FALSE,
+        ],
+        'weight' => 0,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'boolean',
+        'label' => 'above',
+        'weight' => 0,
+        'settings' => [
+          'format' => 'enabled-disabled',
+        ],
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Author'))
+      ->setDescription(t('The user ID of the geo author.'))
+      ->setSetting('target_type', 'user')
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => 60,
+          'placeholder' => '',
+        ],
+        'weight' => 15,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'author',
+        'weight' => 15,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Authored on'))
+      ->setTranslatable(TRUE)
+      ->setDescription(t('The time that the geo was created.'))
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'timestamp',
+        'weight' => 20,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'datetime_timestamp',
+        'weight' => 20,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setTranslatable(TRUE)
+      ->setDescription(t('The time that the geo was last edited.'));
+
+    $fields['label'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Label'))
+      ->setDescription(t('Short human identification or description.'))
+      ->setRequired(TRUE)
+      ->setTranslatable(TRUE)
+      ->setRevisionable(TRUE)
+      ->setSetting('max_length', 1275)
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'string',
+        'weight' => -5,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+        'region' => 'hidden',
+      ])
+      ->setDisplayConfigurable('form', TRUE);
+
+    return $fields;
+  }
+
+}
---
+++ modules/localgov_geo_update/src/Entity/LocalgovGeoType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\localgov_geo\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBundleBase;
+
+/**
+ * Defines the Geo type configuration entity.
+ *
+ * @ConfigEntityType(
+ *   id = "localgov_geo_type",
+ *   label = @Translation("Geo type"),
+ *   bundle_of = "localgov_geo",
+ *   config_prefix = "localgov_geo_type",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "uuid",
+ *     "label_token",
+ *   }
+ * )
+ */
+class LocalgovGeoType extends ConfigEntityBundleBase {
+
+  /**
+   * The machine name of this geo type.
+   *
+   * @var string
+   */
+  protected $id;
+
+  /**
+   * The human-readable name of the geo type.
+   *
+   * @var string
+   */
+  protected $label;
+
+  /**
+   * The token string to use to replace for entity label default.
+   *
+   * @var string
+   */
+  protected $label_token;
+
+  /**
+   * Return the label token.
+   */
+  public function labelToken() {
+    return $this->label_token;
+  }
+
+}
